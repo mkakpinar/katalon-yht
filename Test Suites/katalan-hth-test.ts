ed2ac5cde2cc886584da13b2e46e24683321c2e1
musa.akpinar <musa.akpinar@192.168.1.122>
2022-10-27 23:41:51 +0300

    add test suite

---
+++ Test Suites/katalan-hth-test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>katalan-hth-test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0a9df9c9-fb8d-482f-b5b4-17599edbf38e</testSuiteGuid>
+   <testCaseLink>
+      <guid>7e7a9ed4-4c75-4b28-8402-9fe97752aa31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
